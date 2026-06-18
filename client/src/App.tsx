29b28d49fd9201caf64b8b9d7dfb71665773500d
Lohith <131429805+lohithlalesh@users.noreply.github.com>
2026-06-18 16:45:53 +0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,10 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
 import { Route, Switch, Router as WouterRouter } from "wouter";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Home from "./pages/Home";
 
 function Router() {
   return (
@@ -6,8 +12,24 @@ function Router() {
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/404" component={NotFound} />
+        {/* Final fallback route */}
         <Route component={NotFound} />
       </Switch>
     </WouterRouter>
   );
 }
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="light">
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
